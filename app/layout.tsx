300c17b3a5868c50a305fbe27a06a4394e3738e2
Maleesha Malshan Dissanayaka <124897663+Malshan20@users.noreply.github.com>
2025-10-29 18:51:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,22 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GE31ZBKT11"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GE31ZBKT11');
+`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
